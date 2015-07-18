761e3ed896c4e30082cd74697c6f0acc049869f2
Gautier Nilhcem <nilhcem@gmail.com>
2015-07-19 00:49:36 +0200

    Start creating BookDetailActivity

--- app/src/main/kotlin/com/nilhcem/henripotier/model/Book.kt
+++ app/src/main/kotlin/com/nilhcem/henripotier/model/Book.kt
@@ -1,3 +1,5 @@
 package com.nilhcem.henripotier.model
 
-public data class Book(val isbn: String, val title: String, val price: Float, val cover: String)
+import java.io.Serializable
+
+public data class Book(val isbn: String, val title: String, val price: Float, val cover: String) : Serializable
--- app/src/main/kotlin/com/nilhcem/henripotier/ui/books/list/BooksListActivity.kt
+++ app/src/main/kotlin/com/nilhcem/henripotier/ui/books/list/BooksListActivity.kt
@@ -3,19 +3,20 @@ package com.nilhcem.henripotier.ui.books.list
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.StaggeredGridLayoutManager
-import android.widget.Toast
 import com.nilhcem.henripotier.R
 import com.nilhcem.henripotier.network.RestApi
+import com.nilhcem.henripotier.ui.books.detail.BookDetailActivity
 import kotlinx.android.synthetic.books_list.booksList
 import org.jetbrains.anko.AnkoLogger
 import org.jetbrains.anko.async
+import org.jetbrains.anko.startActivity
 import org.jetbrains.anko.uiThread
 import java.util.ArrayList
 
 public class BooksListActivity : AppCompatActivity(), AnkoLogger {
 
     private val adapter = BooksListAdapter { book ->
-        Toast.makeText(this, "Selected: ${book.title}", Toast.LENGTH_SHORT).show()
+        startActivity<BookDetailActivity>(BookDetailActivity.extraBook to book)
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -23,10 +24,7 @@ public class BooksListActivity : AppCompatActivity(), AnkoLogger {
         setContentView(R.layout.books_list)
         booksList.setLayoutManager(StaggeredGridLayoutManager(2, StaggeredGridLayoutManager.VERTICAL))
         booksList.setAdapter(adapter)
-    }
 
-    override fun onResume() {
-        super<AppCompatActivity>.onResume()
         getBooksList()
     }
 
