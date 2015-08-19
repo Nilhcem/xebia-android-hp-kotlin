65d191a7340410f92fbf2f701e83d2c1e4d98ba5
Gautier Nilhcem <nilhcem@gmail.com>
2015-08-19 11:15:22 +0200

    Use Kotlin Android Extensions

--- app/src/main/kotlin/com/nilhcem/henripotier/ui/cart/CartItem.kt
+++ app/src/main/kotlin/com/nilhcem/henripotier/ui/cart/CartItem.kt
@@ -3,25 +3,22 @@ package com.nilhcem.henripotier.ui.cart
 import android.content.Context
 import android.view.LayoutInflater
 import android.widget.LinearLayout
-import android.widget.TextView
 import com.nilhcem.henripotier.R
 import com.nilhcem.henripotier.core.extensions.toBold
-import org.jetbrains.anko.find
+import kotlinx.android.synthetic.cart_item.view.priceView
+import kotlinx.android.synthetic.cart_item.view.titleView
 import org.jetbrains.anko.text
 
 class CartItem(context: Context) : LinearLayout(context) {
 
-    private val title: TextView
-    private val price: TextView
-
     init {
         LayoutInflater.from(context).inflate(R.layout.cart_item, this, true)
-        title = find(R.id.cart_item_title)
-        price = find(R.id.cart_item_price)
     }
 
     fun bindData(data: CartItemData, titleInBold: Boolean) {
-        title.text = if (titleInBold) data.title.toBold() else data.title
-        price.text = data.price
+        with (data) {
+            titleView.text = if (titleInBold) title.toBold() else title
+            priceView.text = price
+        }
     }
 }
--- app/src/main/kotlin/com/nilhcem/henripotier/ui/list/BooksListItem.kt
+++ app/src/main/kotlin/com/nilhcem/henripotier/ui/list/BooksListItem.kt
@@ -6,9 +6,6 @@ import android.text.Html
 import android.view.LayoutInflater
 import android.view.View
 import android.widget.FrameLayout
-import android.widget.ImageView
-import android.widget.ProgressBar
-import android.widget.TextView
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.resource.drawable.GlideDrawable
 import com.bumptech.glide.request.RequestListener
@@ -16,6 +13,9 @@ import com.bumptech.glide.request.target.Target
 import com.nilhcem.henripotier.R
 import com.nilhcem.henripotier.core.extensions.euroPrice
 import com.nilhcem.henripotier.model.Book
+import kotlinx.android.synthetic.books_list_item.view.coverView
+import kotlinx.android.synthetic.books_list_item.view.loadingView
+import kotlinx.android.synthetic.books_list_item.view.titleView
 import org.jetbrains.anko.find
 import org.jetbrains.anko.text
 import org.jetbrains.anko.visibility
@@ -23,34 +23,30 @@ import org.jetbrains.anko.visibility
 class BooksListItem(context: Context) : FrameLayout(context) {
 
     val card: CardView
-    private val title: TextView
-    private val cover: ImageView
-    private val loading: ProgressBar
 
     init {
         LayoutInflater.from(context).inflate(R.layout.books_list_item, this, true)
-        card = find(R.id.book_card)
-        title = find(R.id.book_title)
-        cover = find(R.id.book_cover)
-        loading = find(R.id.book_loading)
+        card = find(R.id.cardView)
     }
 
     fun bindData(book: Book) {
-        cover.setImageResource(0)
-        cover.visibility = View.VISIBLE
-        loading.visibility = View.VISIBLE
+        with (book) {
+            coverView.setImageResource(0)
+            coverView.visibility = View.VISIBLE
+            loadingView.visibility = View.VISIBLE
 
-        title.text = Html.fromHtml("${book.title}<br><b>${book.price.euroPrice()}</b>")
-        Glide.with(getContext()).load(book.cover).crossFade()
-                .listener(object : RequestListener<String, GlideDrawable> {
-                    override fun onResourceReady(resource: GlideDrawable?, model: String?, target: Target<GlideDrawable>?, isFromMemoryCache: Boolean, isFirstResource: Boolean): Boolean = hideLoading()
-                    override fun onException(e: Exception?, model: String?, target: Target<GlideDrawable>?, isFirstResource: Boolean): Boolean = hideLoading()
-                })
-                .into(cover)
+            titleView.text = Html.fromHtml("${title}<br><b>${price.euroPrice()}</b>")
+            Glide.with(getContext()).load(cover).crossFade()
+                    .listener(object : RequestListener<String, GlideDrawable> {
+                        override fun onResourceReady(resource: GlideDrawable?, model: String?, target: Target<GlideDrawable>?, isFromMemoryCache: Boolean, isFirstResource: Boolean): Boolean = hideLoading()
+                        override fun onException(e: Exception?, model: String?, target: Target<GlideDrawable>?, isFirstResource: Boolean): Boolean = hideLoading()
+                    })
+                    .into(coverView)
+        }
     }
 
     fun hideLoading(): Boolean {
-        loading.visibility = View.GONE
+        loadingView.visibility = View.GONE
         return false
     }
 }
